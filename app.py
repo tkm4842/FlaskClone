be78109473650191cabc544ecf1bf244393a2702
tkm4842 <35785063+tkm4842@users.noreply.github.com>
2018-03-15 15:10:23 -0700

    Update app.py
    dfsfsd

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
-
+  #return render_template('index.html')
+  return 'dfsdfdsfdfs'
 
 @app.route('/about')
 def about():
