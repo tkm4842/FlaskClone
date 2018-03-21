8d1842f4fc389f32db10262ea9d0cc4c2a081d2c
Terry Kim <tkim@siluriatech.com>
2018-03-21 15:40:41 -0700

    port remove

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from bokeh.palettes import all_palettes
 from bokeh.layouts import widgetbox
 from bokeh.models.widgets import CheckboxGroup
 from bokeh.io import output_notebook
+from bokeh.embed import components
+from bokeh.resources import INLINE
 import numpy as np
 
 
@@ -15,12 +17,21 @@ app = Flask(__name__)
 # Set API key
 quandl.ApiConfig.api_key = "sfvyrsyZzjsQaacndRPV"
 
-# Load data set via quandl python api
-data = quandl.get_table('WIKI/PRICES', paginate=True, date = { 'gte': '2018-01-01', 'lte': '2018-03-15' })
-
 # options 
 price_type_list = ['open','close','adj_open','adj_close']
+app.vars={}
 
+def get_data(app_vars):
+	ticker=app_vars['tickersymbol']
+
+	columns =['date']
+	for option in app_vars:
+		if option!='tickersymbol' and app_vars[option]==1:
+			columns.append(option)
+
+	data =  quandl.get_table('WIKI/PRICES', paginate=True,qopts = { 'columns': columns}, date = { 'gte': '2018-03-01', 'lte': '2018-03-20' }, ticker=ticker)
+
+	return data
 	
 def datetime(x):
     return np.array(x, dtype=np.datetime64)
@@ -32,25 +43,26 @@ def getcolordict(price_type_list):
 		colordict[price_type] = colormap[i]
 	return colordict
 
-def create_figure(price_type_list):
+def create_figure(data, ticker):
 	p=figure(x_axis_type='datetime', title='Quandl WIKI EOD Stock Prices')
 	p.grid.grid_line_alpha=0.3
 	p.xaxis.axis_label='Date'
 	p.yaxis.axis_label='Stock Price'
 	
 	colordict= getcolordict(price_type_list)
-	
-	tickers=['GOOG']
-	for ticker in tickers:
-		df = data[data['ticker']==ticker]
-		for price_type in price_type_list:
-			p.line(x=datetime(df['date']), y=df[price_type], color=colordict[price_type], legend=ticker+': '+price_type)
+
+
+	df = data
+	for col in df.columns:
+		if col!='date':
+			p.line(x=datetime(df['date']), y=df[col], color=colordict[col], legend=ticker+': '+col)
 	p.legend.location='top_left'
 	return p
 
 
-@app.route('/')
+@app.route('/index',methods=['GET','POST'])
 def index():
+	'''
 	# Determine the selected option
 	current_pricetype = request.args.get("pricetype")
 	if current_pricetype == None:
@@ -63,13 +75,46 @@ def index():
 	script, div = components(plot)
 	return render_template("quandlWIKIprice.html", script=script, div=div,
 		price_type_list=price_type_list,  current_pricetype=current_pricetype)
-	
+	'''
+	if request.method=='GET':
+		return render_template('WIKIpriceinfo.html')
+	else:
+		# request was a post
+		app.vars['tickersymbol'] = request.form['tickersymbol']
 
-# @app.route('/about')
-def about():
-	return render_template('about.html')
+
+		for option in price_type_list:
+			name = request.form.get(option)
+			if name==None:
+				app.vars[option]=0
+			else:
+
+				app.vars[option] = 1
+
+		ticker = app.vars['tickersymbol']
+		data = get_data(app.vars)
+		p = create_figure(data, ticker)
+
+
+
+		# Embed plot into HTML via Flask Render
+		script, div = components(p)
+		js_resources = INLINE.render_js()
+		css_resources = INLINE.render_css()
+
+		return render_template("end.html",  plot_script=script,  plot_div=div,js_resources=js_resources, css_resources=css_resources)
+
+
+
+
+
 
 if __name__ == '__main__':
-	app.run(port=33507, debug=True)
-	#p=create_figure(price_type_list)
-	#show(p)
+	app.run(debug=True)
+	'''
+	app_vars={'tickersymbol': 'GOOG','open':1,'close':1,'adj_open':1,'adj_close':1}
+	ticker = app_vars['tickersymbol']
+	data = get_data(app_vars)
+	print data
+	p=create_figure(data, ticker)
+	show(p)'''
