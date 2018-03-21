13dbd995c5dd984ff42070af77cb33035812859b
Terry Kim <tkim@siluriatech.com>
2018-03-21 15:42:49 -0700

    port remove pt2

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def index():
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run()
 	'''
 	app_vars={'tickersymbol': 'GOOG','open':1,'close':1,'adj_open':1,'adj_close':1}
 	ticker = app_vars['tickersymbol']
